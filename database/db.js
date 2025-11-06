cdb9b24722d302d299d3993c7895bacb84bc1770
Marcos Salles <marcowsgm@gmail.com>
2025-11-06 16:58:57 -0300

    [SETUP] create initial tables for grupos

--- database/db.js
+++ database/db.js
@@ -12,12 +12,20 @@ const db = new sqlite3.Database(dbPath);
 db.serialize(() => {
     db.run(`
     CREATE TABLE IF NOT EXISTS contatos (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      nome TEXT,
-      telefone TEXT,
-      grupo TEXT
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT,
+        telefone TEXT,
+        grupo TEXT
+        )
+    `);
+
+        db.run(`
+    CREATE TABLE IF NOT EXISTS grupos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT UNIQUE
     )
-  `);
+    `);
+
 });
 
 export default db;
